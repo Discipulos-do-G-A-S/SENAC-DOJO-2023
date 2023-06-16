a1e30923a78aea2f42bebb9aef31d55e6416024e
lucas pedroso <pedrosolucas1745@gmail.com>
2023-06-16 12:56:45 -0300

    subindo versão com js para request de login

--- Models/login.php
+++ Models/login.php
@@ -12,25 +12,26 @@ class dadosLogin {
         $this->$atributo = $valor; 
     } 
 
-    public function validarLogin() { 
-        if($this -> cpf == null || $this -> password == null) { 
-            header('refresh:2.0; http://localhost/SENAC-DOJO-2023/login.html'); 
-        } else { 
-            session_start(); 
-            include('conexao.php'); 
-            $querry = ("select cpf_user,senha_user, id_logins from logins where cpf_user = '" . $this->cpf . "' and senha_user='" . $this->password . "';"); 
-            $sql = mysqli_query($banco, $querry); 
-            $resultado = mysqli_num_rows($sql); 
-            echo($resultado); 
-            if ($resultado == 1) { 
-                $row = mysqli_fetch_row($sql);
-                $_SESSION['id'] = $row[0]; 
-                header('location:http://localhost/SENAC-DOJO-2023/views/inserirProjeto.html'); 
-            } else { 
-                header('refresh:2.0; http://localhost/SENAC-DOJO-2023/views/login.html'); 
-            } 
-            mysqli_close($banco); 
-        } 
-    } 
+    public function validarLogin() {
+        if ($this->cpf == null || $this->password == null) {
+            // header('refresh:2.0; http://localhost/SENAC-DOJO-2023/login.html');
+        } else {
+            session_start();
+            include('conexao.php');
+            $querry = ("select cpf_user, senha_user, id_logins from logins where cpf_user = '" . $this->cpf . "' and senha_user='" . $this->password . "';");
+            $sql = mysqli_query($banco, $querry);
+            $resultado = mysqli_num_rows($sql);
+            if ($resultado == 1) {
+                $registros = array(); // Array to store the records
+                $registro = mysqli_fetch_assoc($sql); // Use mysqli_fetch_assoc to get an associative array
+                $registros[] = $registro;
+                echo json_encode($registros);
+            } else {
+                echo json_encode([]); // Retorna JSON vazio
+            }
+            mysqli_close($banco);
+        }
+    }
+    
 } 
 ?>
